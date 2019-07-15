818ce931da419c11486dd39e25e151b8530dcd32
yjy <yjy@ekuaibao.com>
2019-07-15 19:07:24 +0800

    reformat

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,35 @@
 <template>
   <div id="app">
-    <div  class="fm-container"><router-view/></div>
+    <div class="fm-container">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
+  name: "app",
   methods: {
-    handleHome () {
-      this.$router.push({path: '/'})
+    handleHome() {
+      this.$router.push({ path: "/" });
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
-.fm-header{
+.fm-header {
   height: 50px;
-  box-shadow: 0 2px 10px rgba(70,160,252, 0.6);
+  box-shadow: 0 2px 10px rgba(70, 160, 252, 0.6);
   padding: 0 10px;
-  background-image: linear-gradient(to right,#1278f6,#00b4aa);
+  background-image: linear-gradient(to right, #1278f6, #00b4aa);
   position: relative;
 
-  .fm-logo{
+  .fm-logo {
     height: 26px;
     vertical-align: middle;
   }
-  .fm-title{
+  .fm-title {
     display: inline-block;
     line-height: 50px;
     vertical-align: middle;
@@ -39,41 +40,44 @@ export default {
     margin-left: 6px;
     cursor: pointer;
   }
-  .fm-link{
+  .fm-link {
     height: 50px;
     float: right;
-    
-    a{
+
+    a {
       color: #fff;
       text-decoration: none;
       font-size: 14px;
       line-height: 50px;
       font-weight: 500;
       margin-left: 15px;
-      
-      &:hover{
+
+      &:hover {
         opacity: 0.8;
       }
 
-      &.ad{
+      &.ad {
         color: #f5dab1;
       }
     }
   }
 }
-.fm-container{
+.fm-container {
   height: calc(100% - 50px);
 }
-*, :after, :before {
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
+*,
+:after,
+:before {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
 }
-html,body{
+html,
+body {
   height: 100%;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
