c53c1f22a057ebd3d92ed09c48b78dd2e786701c
yjy <yjy@ekuaibao.com>
2019-07-15 10:29:37 +0800

    fix

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -5,14 +5,19 @@
         <el-aside width="250px">
           <div class="components-list">
             <div class="widget-cate">基础字段</div>
-            <draggable tag="ul" :list="basicComponents" 
+            <draggable
+              tag="ul"
+              :list="basicComponents"
               v-bind="{group:{ name:'people', pull:'clone',put:false},sort:false, ghostClass: 'ghost'}"
               @end="handleMoveEnd"
               @start="handleMoveStart"
               :move="handleMove"
             >
-              
-              <li class="form-edit-widget-label" v-for="(item, index) in basicComponents" :key="index">
+              <li
+                class="form-edit-widget-label"
+                v-for="(item, index) in basicComponents"
+                :key="index"
+              >
                 <a>
                   <i class="icon iconfont" :class="item.icon"></i>
                   <span>{{item.name}}</span>
@@ -21,30 +26,40 @@
             </draggable>
 
             <div class="widget-cate">高级字段</div>
-            <draggable tag="ul" :list="advanceComponents" 
+            <draggable
+              tag="ul"
+              :list="advanceComponents"
               v-bind="{group:{ name:'people', pull:'clone',put:false},sort:false, ghostClass: 'ghost'}"
               @end="handleMoveEnd"
               @start="handleMoveStart"
               :move="handleMove"
             >
-              
-              <li class="form-edit-widget-label" v-for="(item, index) in advanceComponents" :key="index">
+              <li
+                class="form-edit-widget-label"
+                v-for="(item, index) in advanceComponents"
+                :key="index"
+              >
                 <a>
                   <i class="icon iconfont" :class="item.icon"></i>
                   <span>{{item.name}}</span>
                 </a>
               </li>
             </draggable>
-            
+
             <div class="widget-cate">布局字段</div>
-            <draggable tag="ul" :list="layoutComponents" 
+            <draggable
+              tag="ul"
+              :list="layoutComponents"
               v-bind="{group:{ name:'people', pull:'clone',put:false},sort:false, ghostClass: 'ghost'}"
               @end="handleMoveEnd"
               @start="handleMoveStart"
               :move="handleMove"
             >
-              
-              <li class="form-edit-widget-label data-grid" v-for="(item, index) in layoutComponents" :key="index">
+              <li
+                class="form-edit-widget-label data-grid"
+                v-for="(item, index) in layoutComponents"
+                :key="index"
+              >
                 <a>
                   <i class="icon iconfont" :class="item.icon"></i>
                   <span>{{item.name}}</span>
@@ -52,36 +67,75 @@
               </li>
             </draggable>
           </div>
-          
         </el-aside>
         <el-container class="center-container" direction="vertical">
           <el-header class="btn-bar" style="height: 45px;">
-            <slot name="action">
-            </slot>
-            <el-button v-if="upload" type="text" size="medium" icon="el-icon-upload2" @click="handleUpload">导入JSON</el-button>
-            <el-button v-if="clearable" type="text" size="medium" icon="el-icon-delete" @click="handleClear">清空</el-button>
-            <el-button v-if="preview" type="text" size="medium" icon="el-icon-view" @click="handlePreview">预览</el-button>
-            <el-button v-if="generateJson" type="text" size="medium" icon="el-icon-tickets" @click="handleGenerateJson">生成JSON</el-button>
-            <el-button v-if="generateCode" type="text" size="medium" icon="el-icon-document" @click="handleGenerateCode">生成代码</el-button>
+            <slot name="action"></slot>
+            <el-button
+              v-if="upload && false"
+              type="text"
+              size="medium"
+              icon="el-icon-upload2"
+              @click="handleUpload"
+            >导入JSON</el-button>
+            <el-button
+              v-if="clearable"
+              type="text"
+              size="medium"
+              icon="el-icon-delete"
+              @click="handleClear"
+            >清空</el-button>
+            <el-button
+              v-if="preview"
+              type="text"
+              size="medium"
+              icon="el-icon-view"
+              @click="handlePreview"
+            >预览</el-button>
+            <el-button
+              v-if="generateJson"
+              type="text"
+              size="medium"
+              icon="el-icon-tickets"
+              @click="handleGenerateJson"
+            >生成JSON</el-button>
+            <el-button
+              v-if="generateCode"
+              type="text"
+              size="medium"
+              icon="el-icon-document"
+              @click="handleGenerateCode"
+            >生成代码</el-button>
           </el-header>
           <el-main :class="{'widget-empty': widgetForm.Components.length == 0}">
-            
-            <widget-form v-if="!resetJson"  ref="widgetForm" :data="widgetForm" :select.sync="widgetFormSelect"></widget-form>
+            <widget-form
+              v-if="!resetJson"
+              ref="widgetForm"
+              :data="widgetForm"
+              :select.sync="widgetFormSelect"
+            ></widget-form>
           </el-main>
         </el-container>
-        
+
         <el-aside class="widget-config-container">
           <el-container>
             <el-header height="45px">
-              <div class="config-tab" :class="{active: configTab=='widget'}" @click="handleConfigSelect('widget')">字段属性</div>
-              <div class="config-tab" :class="{active: configTab=='form'}" @click="handleConfigSelect('form')">表单属性</div>
+              <div
+                class="config-tab"
+                :class="{active: configTab=='widget'}"
+                @click="handleConfigSelect('widget')"
+              >字段属性</div>
+              <div
+                class="config-tab"
+                :class="{active: configTab=='form'}"
+                @click="handleConfigSelect('form')"
+              >表单属性</div>
             </el-header>
             <el-main class="config-content">
               <widget-config v-show="configTab=='widget'" :data="widgetFormSelect"></widget-config>
               <form-config v-show="configTab=='form'" :data="widgetForm.config"></form-config>
             </el-main>
           </el-container>
-          
         </el-aside>
 
         <cus-dialog
@@ -91,11 +145,18 @@
           width="1000px"
           form
         >
-          <generate-form insite="true" v-if="previewVisible" :data="widgetForm" :value="widgetModels" :remote="remoteFuncs" ref="generateForm">
-
+          <generate-form
+            insite="true"
+            v-if="previewVisible"
+            :data="widgetForm"
+            :value="widgetModels"
+            :remote="remoteFuncs"
+            ref="generateForm"
+          >
             <template v-slot:blank="scope">
-              宽度：<el-input v-model="scope.model.blank.width" style="width: 100px"></el-input>
-              高度：<el-input v-model="scope.model.blank.height" style="width: 100px"></el-input>
+              宽度：
+              <el-input v-model="scope.model.blank.width" style="width: 100px"></el-input>高度：
+              <el-input v-model="scope.model.blank.height" style="width: 100px"></el-input>
             </template>
           </generate-form>
 
@@ -124,9 +185,8 @@
           width="800px"
           form
         >
-          
           <div id="jsoneditor" style="height: 400px;width: 100%;">{{jsonTemplate}}</div>
-          
+
           <template slot="action">
             <el-button type="primary" class="json-btn" :data-clipboard-text="jsonCopyValue">复制数据</el-button>
           </template>
@@ -145,24 +205,27 @@
       </el-container>
     </el-main>
   </el-container>
-  
 </template>
 
 <script>
-import Draggable from 'vuedraggable'
-import WidgetConfig from './WidgetConfig'
-import FormConfig from './FormConfig'
-import WidgetForm from './WidgetForm'
-import CusDialog from './CusDialog'
-import GenerateForm from './GenerateForm'
-import Clipboard from 'clipboard'
-import {basicComponents, layoutComponents, advanceComponents} from './componentsConfig.js'
-import {loadJs, loadCss} from '../util/index.js'
-import request from '../util/request.js'
-import generateCode from './generateCode.js'
+import Draggable from "vuedraggable";
+import WidgetConfig from "./WidgetConfig";
+import FormConfig from "./FormConfig";
+import WidgetForm from "./WidgetForm";
+import CusDialog from "./CusDialog";
+import GenerateForm from "./GenerateForm";
+import Clipboard from "clipboard";
+import {
+  basicComponents,
+  layoutComponents,
+  advanceComponents
+} from "./componentsConfig.js";
+import { loadJs, loadCss } from "../util/index.js";
+import request from "../util/request.js";
+import generateCode from "./generateCode.js";
 
 export default {
-  name: 'fm-making-form',
+  name: "fm-making-form",
   components: {
     Draggable,
     WidgetConfig,
@@ -185,7 +248,7 @@ export default {
       default: false
     },
     upload: {
-      type: Boolean, 
+      type: Boolean,
       default: false
     },
     clearable: {
@@ -193,7 +256,7 @@ export default {
       default: false
     }
   },
-  data () {
+  data() {
     return {
       basicComponents,
       layoutComponents,
@@ -203,43 +266,45 @@ export default {
         Components: [],
         config: {
           labelWidth: 100,
-          labelPosition: 'right',
-          size: 'small'
-        },
+          labelPosition: "right",
+          size: "small"
+        }
       },
-      configTab: 'widget',
+      configTab: "widget",
       widgetFormSelect: null,
       previewVisible: false,
       jsonVisible: false,
       codeVisible: false,
       uploadVisible: false,
       remoteFuncs: {
-        func_test (resolve) {
+        func_test(resolve) {
           setTimeout(() => {
             const options = [
-              {id: '1', name: '1111'},
-              {id: '2', name: '2222'},
-              {id: '3', name: '3333'}
-            ]
+              { id: "1", name: "1111" },
+              { id: "2", name: "2222" },
+              { id: "3", name: "3333" }
+            ];
 
-            resolve(options)
-          }, 2000)
+            resolve(options);
+          }, 2000);
         },
-        funcGetToken (resolve) {
-          request.get('http://tools-server.xiaoyaoji.cn/api/uptoken').then(res => {
-            resolve(res.uptoken)
-          })
+        funcGetToken(resolve) {
+          request
+            .get("http://tools-server.xiaoyaoji.cn/api/uptoken")
+            .then(res => {
+              resolve(res.uptoken);
+            });
         },
-        upload_callback (response, file, fileList) {
-          console.log('callback', response, file, fileList)
+        upload_callback(response, file, fileList) {
+          console.log("callback", response, file, fileList);
         }
       },
       widgetModels: {},
-      blank: '',
-      htmlTemplate: '',
-      jsonTemplate: '',
+      blank: "",
+      htmlTemplate: "",
+      jsonTemplate: "",
       uploadEditor: null,
-      jsonCopyValue: '',
+      jsonCopyValue: "",
       jsonClipboard: null,
       jsonEg: `{
   "Components": [
@@ -288,142 +353,141 @@ export default {
     "size": "small"
   }
 }`
-    }
+    };
   },
-  mounted () {
-
-  },
+  mounted() {},
   methods: {
-    handleGoGithub () {
-      window.location.href = 'https://github.com/GavinZhuLei/vue-form-making'
+    handleGoGithub() {
+      window.location.href = "https://github.com/GavinZhuLei/vue-form-making";
     },
-    handleConfigSelect (value) {
-      this.configTab = value
+    handleConfigSelect(value) {
+      this.configTab = value;
     },
-    handleMoveEnd (evt) {
-      console.log('end', evt)
+    handleMoveEnd(evt) {
+      console.log("end", evt);
     },
-    handleMoveStart ({oldIndex}) {
-      console.log('start', oldIndex, this.basicComponents)
+    handleMoveStart({ oldIndex }) {
+      console.log("start", oldIndex, this.basicComponents);
     },
-    handleMove () {
-      return true
+    handleMove() {
+      return true;
     },
-    handlePreview () {
-      console.log(this.widgetForm)
-      this.previewVisible = true
+    handlePreview() {
+      console.log(this.widgetForm);
+      this.previewVisible = true;
     },
-    handleTest () {
-      this.$refs.generateForm.getData().then(data => {
-        this.$alert(data, '').catch(e=>{})
-        this.$refs.widgetPreview.end()
-      }).catch(e => {
-        this.$refs.widgetPreview.end()
-      })
+    handleTest() {
+      this.$refs.generateForm
+        .getData()
+        .then(data => {
+          this.$alert(data, "").catch(e => {});
+          this.$refs.widgetPreview.end();
+        })
+        .catch(e => {
+          this.$refs.widgetPreview.end();
+        });
     },
-    handleReset () {
-      this.$refs.generateForm.reset()
+    handleReset() {
+      this.$refs.generateForm.reset();
     },
-    handleGenerateJson () {
-      this.jsonVisible = true
-      this.jsonTemplate = this.widgetForm
-      console.log("-----------------------------")
-      console.log("-----------------------------")
-      console.log("-----------------------------")
-      console.log("-----------------------------")
-      console.log("-----------------------------")
-      console.log(JSON.stringify(this.widgetForm))
-      console.log("-----------------------------")
-      console.log("-----------------------------")
-      console.log("-----------------------------")
-      console.log("-----------------------------")
-      console.log("-----------------------------")
-      console.log("-----------------------------")
+    handleGenerateJson() {
+      this.jsonVisible = true;
+      this.jsonTemplate = this.widgetForm;
+      console.log("-----------------------------");
+      console.log("-----------------------------");
+      console.log("-----------------------------");
+      console.log("-----------------------------");
+      console.log("-----------------------------");
+      console.log(JSON.stringify(this.widgetForm));
+      console.log("-----------------------------");
+      console.log("-----------------------------");
+      console.log("-----------------------------");
+      console.log("-----------------------------");
+      console.log("-----------------------------");
+      console.log("-----------------------------");
 
       this.$nextTick(() => {
-
         //const editor = ace.edit('jsoneditor')
         //editor.session.setMode("ace/mode/json")
 
         if (!this.jsonClipboard) {
-          this.jsonClipboard = new Clipboard('.json-btn')
-          this.jsonClipboard.on('success', (e) => {
-            this.$message.success('复制成功')
-          })
+          this.jsonClipboard = new Clipboard(".json-btn");
+          this.jsonClipboard.on("success", e => {
+            this.$message.success("复制成功");
+          });
         }
-        this.jsonCopyValue = JSON.stringify(this.widgetForm)
-      })
+        this.jsonCopyValue = JSON.stringify(this.widgetForm);
+      });
     },
-    handleGenerateCode () {
-      this.codeVisible = true
-      this.htmlTemplate = generateCode(JSON.stringify(this.widgetForm))
+    handleGenerateCode() {
+      this.codeVisible = true;
+      this.htmlTemplate = generateCode(JSON.stringify(this.widgetForm));
       this.$nextTick(() => {
         //const editor = ace.edit('codeeditor')
         //editor.session.setMode("ace/mode/html")
-      })
+      });
     },
-    handleUpload () {
-      this.uploadVisible = true
+    handleUpload() {
+      this.uploadVisible = true;
       this.$nextTick(() => {
-        this.uploadEditor = ace.edit('uploadeditor')
-        this.uploadEditor.session.setMode("ace/mode/json")
-      })
+        //this.uploadEditor = ace.edit("uploadeditor");
+        //this.uploadEditor.session.setMode("ace/mode/json");
+      });
     },
-    handleUploadJson () {
-      try {
-        this.setJSON(JSON.parse(this.uploadEditor.getValue()))
-        this.uploadVisible = false
-      } catch (e) {
-        this.$message.error(e.message)
-        this.$refs.uploadJson.end()
-      }
+    handleUploadJson() {
+      //try {
+        //this.setJSON(JSON.parse(this.uploadEditor.getValue()));
+       // this.uploadVisible = false;
+     // } catch (e) {
+      //  this.$message.error(e.message);
+      //  this.$refs.uploadJson.end();
+      //}
     },
-    handleClear () {
+    handleClear() {
       this.widgetForm = {
         Components: [],
         config: {
           labelWidth: 100,
-          labelPosition: 'right',
-          size: 'small',
-          customClass: ''
-        },
-      }
+          labelPosition: "right",
+          size: "small",
+          customClass: ""
+        }
+      };
 
-      this.widgetFormSelect = {}
+      this.widgetFormSelect = {};
     },
-    getJSON () {
-      return this.widgetForm
+    getJSON() {
+      return this.widgetForm;
     },
-    getHtml () {
-      return generateCode(JSON.stringify(this.widgetForm))
+    getHtml() {
+      return generateCode(JSON.stringify(this.widgetForm));
     },
-    setJSON (json) {
-      this.widgetForm = json
+    setJSON(json) {
+      this.widgetForm = json;
 
-      if (json.list.length> 0) {
-        this.widgetFormSelect = json.list[0]
+      if (json.list.length > 0) {
+        this.widgetFormSelect = json.list[0];
       }
     },
-    handleInput (val) {
-      console.log(val)
-      this.blank = val
+    handleInput(val) {
+      console.log(val);
+      this.blank = val;
     }
   },
   watch: {
     widgetForm: {
       deep: true,
-      handler: function (val) {
-        console.log(this.$refs.widgetForm)
+      handler: function(val) {
+        console.log(this.$refs.widgetForm);
       }
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
-.widget-empty{
-  background: url('../assets/form_empty.png') no-repeat;
+.widget-empty {
+  background: url("../assets/form_empty.png") no-repeat;
   background-position: 50%;
 }
-
 </style>
